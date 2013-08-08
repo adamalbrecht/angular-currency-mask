01983c7226f46cf37943be5f9c046f665fa6b599
Adam Albrecht <adam.albrecht@gmail.com>
2013-08-08 16:42:33 -0400

    Much cleaner and shorter overhaul of the code

--- angular-currency-mask.js
+++ angular-currency-mask.js
@@ -2,50 +2,28 @@ angular.module('currencyMask', []).directive('currencyMask', function () {
   return {
     restrict: 'A',
     require: 'ngModel',
-    link: function (scope, element, attrs, controller) {
-      var formattedValue;
-      var decimalValue;
-
-      var numberWithCommas = function(x, addTrailingZero) {
-        if (!x) { return "" };
-        var parts = x.toString();
-        parts = parts.replace(/[^0-9\.]/g, "");
-        parts = parts.split(".");
+    link: function (scope, element, attrs, ngModelController) {
+      // Run formatting on keyup
+      var numberWithCommas = function(value) {
+        value = value.toString();
+        value = value.replace(/[^0-9\.]/g, "");
+        var parts = value.split('.');
         parts[0] = parts[0].replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, "$&,");
         if (parts[1] && parts[1].length > 2) {
-          parts[1] = parts[1].substring(0,2);
-        }
-        if ((addTrailingZero == true) && (parts.length === 2) && (parts[1].length === 1)) {
-          parts[1] = parts[1] + "0";
+          parts[1] = parts[1].substring(0, 2);
         }
         return parts.join(".");
-      }
-
-      var toDecimal = function(x) {
-        if (!x) { return null; }
-        x = x.replace(/[^0-9\.]/g, "");
-        parts = x.split(".");
-        if (parts.length > 1) {
-          parts[1] = parts[1].substring(0,2);
-        }
-        x = parts.join('.');
-        return parseFloat(x);
-      }
-
-      controller.$render = function() {
-        element.val(formattedValue);  
-      }
-
-      runUpdates = function(addTrailingZero) {
-        scope.$apply(function(){
-          decimalValue = toDecimal(element.val());
-          formattedValue = numberWithCommas(element.val(), addTrailingZero);
-          controller.$setViewValue(decimalValue);
-          controller.$render();
-        });
       };
-
-      // Run formatting on keyup
+      var applyFormatting = function() {
+        var value = element.val();
+        var original = value;
+        if (!value || value.length == 0) { return }
+        value = numberWithCommas(value);
+        if (value != original) {
+          element.val(value);
+          element.triggerHandler('input')
+        }
+      };
       element.bind('keyup', function(e) {
         var keycode = e.keyCode;
         var isTextInputKey = 
@@ -55,13 +33,25 @@ angular.module('currencyMask', []).directive('currencyMask', function () {
           (keycode > 95 && keycode < 112)  || // numpad keys
           (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
           (keycode > 218 && keycode < 223);   // [\]' (in order)
-        if (isTextInputKey) { runUpdates(false); }
+        if (isTextInputKey) {
+          applyFormatting();
+        }
       });
-      // Add extra decimals if needed on blur
-      element.bind('blur', function() {
-        runUpdates(true);
+      ngModelController.$parsers.push(function(value) {
+        if (!value || value.length == 0) {
+          return value;
+        }
+        value = value.toString();
+        value = value.replace(/[^0-9\.]/g, "");
+        return value;
       });
-
+      ngModelController.$formatters.push(function(value) {
+        if (!value || value.length == 0) {
+          return value;
+        }
+        value = numberWithCommas(value);
+        return value;
+      });
     }
   };
 });
